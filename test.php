bc00481e3b3557af5befb40f0655adcf6b73d5af
hilminurhadi <35126818+hilminurhadi@users.noreply.github.com>
2018-01-05 11:54:20 +0700

    Update test.php

--- test.php
+++ test.php
@@ -11,15 +11,15 @@ function telkbomb($no, $jum, $wait){
         curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36');
         $server_output = curl_exec ($ch);
         curl_close ($ch);
-        echo $server_output."\n<font color='green'>Delay: 60 Second</font>";
+        echo $server_output."\n[#008000]Delay: 60 Second\n";
         sleep($wait);
         $x++;
         flush();
     }
 }
-print "\n\n<font color='red'>SMS BOMBER My Telkomsel</font>\n";
-print "<font color='blue'>Unlimited</font>\n";
-print "<font color='green'>.Koe Nurf.</font>\n\n\n";
+print "\n\n[#FF0000]SMS BOMBER My Telkomsel\n";
+print "[#1E90FF]Unlimited\n";
+print "[#008000]Koe Nurf\n\n\n";
 
 echo "Nomor (eg : 628xxx)\nInput : ";
 $nomor = trim(fgets(STDIN));
