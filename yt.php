7770b41f77f557581bf4ffa31ee0161e6021266d
hilminurhadi <35126818+hilminurhadi@users.noreply.github.com>
2019-02-03 10:43:15 +0700

    Create yt.php

---
+++ yt.php
@@ -0,0 +1,39 @@
+<?php
+# koenurf
+# fb.com/mi.nurf
+
+$date = date("Y-m-d");
+
+function yt($channel,$jumlah,$jeda){
+$headers = array("Host: inscritosyt.websiteseguro.com", 
+"Connection: Keep-Alive", 
+"Contect-Length: 51",  
+"Content-Type: application/x-www-form-urlencoded"
+);
+    $x = 0; 
+    while($x < $jumlah) {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "https://inscritosyt.websiteseguro.com/iniciar.php");
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
+        curl_setopt($ch, CURLOPT_POST, TRUE);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, "#tipo:3#<->#".$date."#<->#".$channel);
+    
+        $output = curl_exec($ch);
+        curl_close($ch);
+        echo $output."\n";
+        sleep($jeda);
+        $x++;
+        flush();    
+    }
+}
+echo "Channel YT: ";
+$channel = trim(fgets(STDIN));
+echo "Jumlah: ";
+$jumlah = trim(fgets(STDIN));
+echo "Jeda: ";
+$jeda = trim(fgets(STDIN));
+
+$do = yt($channel,$jumlah,$jeda);
+print $do;
+?>
